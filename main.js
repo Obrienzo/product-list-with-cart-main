218fa484f8d199d1bfdcd2926da3862aa6f6300d
Disney <disneyconstant996@gmail.com>
2025-02-07 21:56:29 +0300

    Add new constructor obj

---
+++ main.js
@@ -0,0 +1,6 @@
+import { DessertMenu } from "./modules/dessertMenu.js";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const menu = new DessertMenu();
+    menu.initialize();
+})
